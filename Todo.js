ec8aa63a128533edc2a13de2de5447cd08cb1888
Kritithakur14 <thakurkriti514@gmail.com>
2024-07-28 12:31:57 +0530

    first commit

---
+++ Todo.js
@@ -0,0 +1,22 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add() {
+
+  if (inputs.value == "") {
+    alert("Please Enter Task");
+
+  } 
+
+  else {
+    
+    let newEle = document.createElement("ul"); //creating ul element
+    newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`; //adding input value to inner HTML of ul
+    text.appendChild(newEle); //adding new element to the text
+    inputs.value = "";
+    newEle.querySelector("i").addEventListener("click", remove);
+    function remove() {
+      newEle.remove();
+    }
+  }
+}
