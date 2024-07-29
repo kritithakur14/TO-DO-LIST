28bb0bbda48b5102cd33454043d9c9b386ae4df9
Kritithakur14 <thakurkriti514@gmail.com>
2024-07-29 13:06:00 +0530

    added comments

--- Todo.js
+++ Todo.js
@@ -12,7 +12,7 @@ function Add() {
     
     let newEle = document.createElement("ul"); //creating ul element
     newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`; //adding input value to inner HTML of ul
-    text.appendChild(newEle); //adding new element to the text
+    text.appendChild(newEle); //adding new elements to the text
     inputs.value = "";
     newEle.querySelector("i").addEventListener("click", remove);
     function remove() {
